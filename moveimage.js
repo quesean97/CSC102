cf2819a2b6fdb5b3c8c4f729123a4346dab428ae
quesean97 <queseanp@gmail.com>
2026-04-25 17:33:06 -0700

    Add files via upload

--- moveimage.js
+++ moveimage.js
@@ -8,11 +8,13 @@ var startLeft = 100;
 var dLeft = 15;
 var dTop = 15;
 
-// creating the function for the image to move within the screen
+// creating the function for the start button to work
 function startInterval()
 {
     intervalidID = setInterval(moveIT, 100);
 }
+
+//when the button is clicked the action of moving the image will follow these rules 
 function moveIT()
 {
     var imageBall = document.getElementById("imgBall");
@@ -21,19 +23,19 @@ function moveIT()
 //tells the page to show the values of the top and left location in pixels
     document.getElementById("msg").innerHTML = "top = " + imageBall.style.top + " left = " + imageBall.style.left;
 // when the image reaches the limit then begin the inverse of the change from left to right to right to left 
-    if ((startLeft + imageBall.width >= window.innerWidth) || (startLeft <=0))
+    if ((startLeft + imageBall.width >= window.innerWidth) || (startLeft <= 0))
     {
         dLeft = -dLeft;
     }
 // when the image reaches the limit then begin the inverse of the change from top to bottom to bottom to top
-    if ((startTop + imageBall.height >= window.innerHeight) || (startTop <=0))
+    if ((startTop + imageBall.height >= window.innerHeight) || (startTop <= 0))
     {
         dTop = -dTop;
     }
     startTop += dTop;
     startLeft += dLeft;
 }
-
+//allow stop button to stop the image from moving 
 function stopInterval()
 {
     clearInterval(intervalidID);
